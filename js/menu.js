b18fe590e77ab5132aca31bc8e879705a2d46e53
Willsanc146 <william93sb@gmail.com>
2023-01-18 11:52:39 -0500

    version 0.0002

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,8 @@
-$(".menu-icon").click(function(){
-    $(this).toggleClass("active");
-    $(".navigation-menu").toggleClass("active");
-    $(".menu-icon i").toggleClass("fa-times");
+$('.menu-icon').click(function () {
+	$(this).toggleClass('active');
+	$('.navigation-menu').toggleClass('active');
+	$('.menu-icon i').toggleClass('fa-times');
 });
+alert(
+	"Para iniciar a jugar por favor ingresa a 'AGREGAR NUEVA PALABRA', agrega todas las palabras con las que quieras jugar."
+);
