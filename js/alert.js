22bc7bc21c825b1e7d11bc1078f85cae7534a4b2
Willsanc146 <william93sb@gmail.com>
2023-01-21 20:12:04 -0500

    V 1.0015

--- js/alert.js
+++
@@ -1,8 +0,0 @@
-function checkLocalStorage() {
-	if (!localStorage.getItem('words')) {
-		const alert = document.createElement('div');
-		alert.classList.add('alert');
-		alert.innerHTML = 'No hay palabras en la lista';
-		document.querySelector('[data-list]').appendChild(alert);
-	}
-}
--- js/readWord.js
+++ js/readWord.js
@@ -4,6 +4,11 @@ export const displayWords = () => {
 	const list = document.querySelector('[data-list]');
 
 	const wordList = JSON.parse(localStorage.getItem('words')) || [];
+	if (wordList.length === 0) {
+		alert(
+			'No hay palabras guardadas. Por favor agregue alguna en la página de agregar palabra.'
+		);
+	}
 	const uniqueArr = [];
 	wordList.forEach((word) => {
 		list.appendChild(createWords(word));
